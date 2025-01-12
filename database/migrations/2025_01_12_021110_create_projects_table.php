e52b1f9593bca5d80c371b7371038bfdcffc0d60
Trinh Nhu Khang Le <nhukhang1102@gmail.com>
2025-01-11 18:18:53 -0800

    set up project and task table

--- database/migrations/2025_01_12_021110_create_projects_table.php
+++ database/migrations/2025_01_12_021110_create_projects_table.php
@@ -6,21 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['open', 'in_progress', 'completed']);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('projects');
     }
--- database/migrations/2025_01_12_021110_create_tasks_table.php
+++ database/migrations/2025_01_12_021110_create_tasks_table.php
@@ -6,21 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('project_id')->constrained()->onDelete('cascade');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('assigned_to')->nullable();
+            $table->date('due_date')->nullable();
+            $table->enum('status', ['to_do', 'in_progress', 'done']);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('tasks');
     }
