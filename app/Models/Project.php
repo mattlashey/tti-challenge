f22659716bb8d6c469baa2befcfeb90d14229b6a
Jason Ho <git@hogt.me>
2025-01-14 22:33:00 -0800

    db: models: implement project/task mass fields and relationships

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Database\Factories\ProjectFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
@@ -14,8 +15,15 @@ class Project extends Model
     const STATUS_IN_PROGRESS = 'in_progress';
     const STATUS_COMPLETED = 'completed';
 
+    protected $guarded = [];
+
     protected static function newFactory()
     {
         return ProjectFactory::new();
+    }
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
     }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Database\Factories\TaskFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Task extends Model
 {
@@ -14,8 +15,15 @@ class Task extends Model
     const STATUS_IN_PROGRESS = 'in_progress';
     const STATUS_DONE = 'done';
 
+    protected $guarded = [];
+
     protected static function newFactory()
     {
         return TaskFactory::new();
+    }
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
     }
 }
