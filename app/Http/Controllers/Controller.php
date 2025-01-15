2959ff681bc600bf93acff43989346fc94e924a8
Jason Ho <git@hogt.me>
2025-01-14 23:10:23 -0800

    handle errors gracefully always, catch validation errors and return a common validation failure message

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,9 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    protected static function error($msg = 'An error has occurred.') {
+        return array(
+            'error' => $msg,
+        );
+    }
 }
