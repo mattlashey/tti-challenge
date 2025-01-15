bc1a1bda6fda434641c06e6de6a0b485be4ad448
Jason Ho <git@hogt.me>
2025-01-14 23:39:56 -0800

    test: isolate challenge requirements from non-challenge requirements

---
+++ tests/Feature/ProjectTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Testing\Fluent\AssertableJson;
+
+test('test projects list task', function () {
+    $project = \App\Models\Project::factory()->create()->first();
+    $tasks = \App\Models\Task::factory()->count(10)->create([
+        'project_id' => $project->id,
+    ]);
+
+    $response = $this->get('/api/projects/'.$project->id.'/tasks');
+    $response->assertStatus(200);
+
+    $response->assertJson(function (AssertableJson $json) use ($project, $tasks) {
+        $json->has('data', null, function(AssertableJson $data) use ($project, $tasks) {
+            isTask($data)
+                ->where('project_id', $project->id)
+                ->etc();
+        })->etc();
+    });
+});
